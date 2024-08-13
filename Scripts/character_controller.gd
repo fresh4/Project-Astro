6975cd82007fbb94d4e6760c519a36088e4bd60d
fresh4 <fresh1736@gmail.com>
2024-08-13 18:17:39 -0500

    Camera controller + movement follows camera direction

--- Scripts/character_controller.gd
+++ Scripts/character_controller.gd
@@ -1,19 +1,38 @@
 extends RigidBody3D
 
-const SPEED = 10
-const MAX_SPEED = 5
-const DECELERATION_SPEED = 2
+const SPEED: float = 5.0
+const MAX_SPEED: float = 5.0
+const DECELERATION_SPEED: float = 2.0
 
 @onready var particles: GPUParticles3D = $Particles
+@onready var camera_arm: SpringArm3D = $SpringArm3D
 
 func _physics_process(_delta: float) -> void:
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var vertical_dir := Input.get_axis("Down", "Up")
 	var direction := (transform.basis * Vector3(input_dir.x, vertical_dir, input_dir.y)).normalized()
+	
+	var forward: Vector3 = (-camera_arm.global_transform.basis.z*Vector3(1, 0, 1)).normalized()
+	var side = (-camera_arm.global_transform.basis.x*Vector3(1, 0, 1)).normalized()
+	var up = Vector3(0, vertical_dir, 0).normalized()
+	
+	# TODO: See about refactoring to eliminate conditionals
 	if direction:
-		apply_central_force(direction * SPEED)
+		var final_direction: Vector3 = Vector3.ZERO
+		if Input.is_action_pressed("Forward"):
+			final_direction += forward
+		elif Input.is_action_pressed("Backward"):
+			final_direction += -forward
+		if Input.is_action_pressed("Left"):
+			final_direction += side
+		elif Input.is_action_pressed("Right"):
+			final_direction += -side
+		if Input.is_action_pressed("Up") or Input.is_action_pressed("Down"):
+			final_direction += up
+		apply_central_force(final_direction * SPEED)
+		
 		particles.emitting = true
-		(particles.process_material as ParticleProcessMaterial).direction = -direction
+		(particles.process_material as ParticleProcessMaterial).direction = -final_direction
 	else:
 		particles.emitting = false
 		if linear_velocity.length() > 0:
